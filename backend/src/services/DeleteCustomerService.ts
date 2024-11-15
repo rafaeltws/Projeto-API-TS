5f6f53dc5736e1ade3510c6d1bd89c2eab46e541
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2024-11-15 04:49:51 -0300

    Add files via upload

---
+++ backend/src/services/DeleteCustomerService.ts
@@ -0,0 +1,34 @@
+import prismaClient from "../prisma";
+
+interface DeleteCustomerProps{
+    id: string;
+}
+
+class DeleteCustomerService {
+    async execute({ id }: DeleteCustomerProps) {
+
+        if(!id) {
+            throw new Error("Solicitação invalida")
+        }
+
+        const findCustomer = await prismaClient.customer.findFirst({
+            where: {
+                id: id
+            }
+        })
+
+        if(!findCustomer) {
+            throw new Error("Cliente inexistente!")
+        }
+
+        await prismaClient.customer.delete({
+            where: {
+                id: findCustomer.id
+            }
+        })
+
+        return { message: "Deletado com sucesso!" }
+    }
+}
+
+export { DeleteCustomerService }
